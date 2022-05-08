bb90c222b85b70c329f004766576759759e985dc
Mirmukhammad Mirsodikov <49703214+mir-mirsodikov@users.noreply.github.com>
2022-05-07 21:30:50 -0500

    Get all survey questions

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import admin from './admin';
+import survey from './survey';
 
 const router = Router();
 
 router.use('/admin', admin);
+router.use('/survey', survey);
 
 export default router;
--- src/routes/survey.ts
+++ src/routes/survey.ts
@@ -1,7 +1,23 @@
 import { Router, Request, Response } from 'express';
+import prisma from '../db';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-  
+router.get('/', async (req: Request, res: Response) => {
+  const surveys = await prisma.survey.findMany({
+    where: {
+      active: true,
+    },
+  });
+
+  const values = surveys.map((survey) => {
+    return {
+      id: survey.id,
+      value: survey.value,
+    }
+  });
+
+  res.json(values);
 });
+
+export default router;
